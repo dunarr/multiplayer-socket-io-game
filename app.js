8b05dc3b10c86e95c07cfd55db6b1de39d5e6da3
william <willef1999@gmail.com>
2019-02-19 23:09:05 +0100

    v1

---
+++ app.js
@@ -0,0 +1,33 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+// Chargement du fichier index.html affiché au client
+var server = http.createServer(function(req, res) {
+});
+
+// Chargement de socket.io
+var io = require('socket.io').listen(server);
+
+// Quand un client se connecte, on le note dans la console
+io.sockets.on('connection', function (socket) {
+  socket.emit('connect_message', 'Veuillez renseigner un pseudo');
+  socket.on('create_block', function (message) {
+    socket.broadcast.emit('created_block',message);
+  });
+  socket.on('login', function (message) {
+    socket.userName = message
+    console.log(message+' est connecté !');
+    socket.broadcast.emit('new_user',message);
+  });
+  socket.on('update_pos', function (message) {
+    socket.broadcast.emit('user_moove',message);
+  });
+  socket.on('disconnect', function(){
+    socket.broadcast.emit('user_quit',socket.userName);
+  });
+});
+
+
+
+server.listen(8001);
